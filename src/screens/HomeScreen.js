f3beedf4934ec5a698a852d58e9661a8fce4a0e0
ht <842932540@qq.com>
2022-02-22 18:44:16 +0800

    添加react-native-vector-icons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {Container} from '../components';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
 const HomeScreen = () => {
   return (
     <Container>
       <View>
         <Text>HomeScreen</Text>
+        <Icon name={'ios-sad'} size={100} color={'blue'} />
       </View>
     </Container>
   );
